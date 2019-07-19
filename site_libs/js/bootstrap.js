9a98d992c40da01b283ded2c638750474ee535db
JulianneZech <36776716+JulianneZech@users.noreply.github.com>
2019-07-19 11:49:38 -0400

    Update bootstrap.js

--- site_libs/js/bootstrap.js
+++ site_libs/js/bootstrap.js
@@ -8,6 +8,10 @@ if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
 }
 
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
+
 +function ($) {
   'use strict';
   var version = $.fn.jquery.split(' ')[0].split('.')
